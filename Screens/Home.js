3e1f3cc4ca58a83f08d57cbdf1f766f5210009af
Birand Kılınç <pacmaidanapac@gmail.com>
2025-03-22 21:23:20 +0300

    Home to Review Det navigation is implemented

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,11 +1,19 @@
-import { View, Text,StyleSheet } from 'react-native'
+import { View, Text,Button } from 'react-native'
 import React from 'react'
 import { globalStyles } from '../styles/Global'
 
-export default function Home() {
+export default function Home({navigation}) {
+  
+  const pressHandler=()=>
+  {
+    navigation.navigate("ReviewDetails"); 
+    //navigation.push("ReviewDetails");
+  }
+  
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Home</Text>
+      <Button title="Go to Details" onPress={pressHandler} />
     </View>
   )
 }
