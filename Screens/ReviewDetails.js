66c1c10ec5771908fdf58bb746637bb93561e736
Birand Kılınç <pacmaidanapac@gmail.com>
2025-03-22 21:27:27 +0300

    Navigate to Review -> Home

--- Screens/ReviewDetails.js
+++ Screens/ReviewDetails.js
@@ -1,10 +1,18 @@
-import { View, Text,StyleSheet } from 'react-native'
+import { View, Text,Button } from 'react-native'
 import React from 'react'
 import { globalStyles } from '../styles/Global'
-export default function ReviewDetails() {
+export default function ReviewDetails({navigation}) {
+
+  const pressHandler=()=>
+  {
+    navigation.goBack();
+  }
+
+
   return (
     <View style={globalStyles.container}>
       <Text>ReviewDetails</Text>
+      <Button title="Back to Home Screen" onPress={pressHandler}/>
     </View>
   )
 }
