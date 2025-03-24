dcce67d8c1c592dd93798fb12062fff8833e11a7
Birand Kılınç <pacmaidanapac@gmail.com>
2025-03-24 22:09:45 +0300

    Images are implmeneted to ratings according to its value

--- Screens/ReviewDetails.js
+++ Screens/ReviewDetails.js
@@ -1,21 +1,36 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import React from 'react';
-import { globalStyles } from '../styles/Global';
+import { globalStyles,images } from '../styles/Global';
 import Card from '../Shared/Card';
 
 
 export default function ReviewDetails({ route, navigation }) {
   // ✅ Use route.params instead of navigation.getParam()
   const { title, body, rating } = route.params;
-
   return (
     <View style={globalStyles.container}>
       <Card>
       <Text>{title}</Text>
       <Text>{body}</Text>
-      <Text>{rating}</Text>
+      <View style={styles.rating}>
+        <Text>Game Zone Rating: </Text>
+        <Image source={images.ratings[rating]} />
+      </View>
       </Card>
      
     </View>
   );
 }
+const styles=StyleSheet.create(
+  {
+    rating:
+    {
+      flexDirection:"row",
+      justifyContent:"center",
+      paddingTop:16,
+      marginTop:16,
+      borderTopWidth:1,
+      borderTopColor:"#eee",
+    }
+  }
+)
